7e8105756742fbea832d88f30f3e4c16f2b01ffd
Kevin Curtin <kevincurtin88@gmail.com>
2012-11-29 09:59:58 -0500

    getting there

--- lib/proto/scraper.rb
+++ lib/proto/scraper.rb
@@ -1,17 +1,21 @@
 module Proto
   class Scraper
-    attr_accessor :doc, :url_collection
+    attr_accessor :url, :doc, :url_collection
 
     def initialize(url)
+      @url = url.chomp '/'
       @doc = Nokogiri::HTML(open(url))
     end
 
     def collect_urls(selector)
-      @url_collection = doc.css(selector).map(&:link['href'])
+      @url_collection = doc.css(selector).map do |link|
+        "#{url}#{link['href']}"
+      end
     end
 
     def fetch(name='Type', args)
       if url_collection
+        puts 'fetching the collection...'
         visit_urls_and_fetch(args)
       else
         attributes = scrape_attribute_data(args)
@@ -22,26 +26,32 @@ module Proto
     alias_method :fetch_and_create!, :fetch
 
   private
-    def scrape_attribute_data(attributes)
-      length_of_scrape = @doc.css(attributes.first[1]).count
 
+    def visit_urls_and_fetch(attributes)
+      final_array = url_collection.map do |url|
+        page  = Nokogiri::HTML(open(url))
+        attrs_hash = scrape_attribute_data(page, attributes)
+      end
+      create_return_objects('Bee',final_array)
+    end
+    
+    def gather_data
+      job_hash = args.each_with_object({}) do |(key, selector), attrs|
+        attrs[var_name] = job_doc.css(selector).text.strip
+      end
+
+      job_hash
+    end
+
+    def scrape_attribute_data(document=self.doc, attributes)
+      length_of_scrape = document.css(attributes.first[1]).count
       final_array = length_of_scrape.times.map do |index|
         attributes.inject(Hash.new) do |hash, (attr_name, selector)|
-          hash.merge(attr_name => @doc.css(selector)[index].text.strip) if doc.css(selector)[index]
+          hash.merge(attr_name => document.css(selector)[index].text.strip) if document.css(selector)[index]
         end
       end
 
       final_array.compact
-    end
-
-    def visit_urls_and_fetch(attributes)
-      url_collection.each do |url|
-        page  = Nokogiri::HTML(open(url))
-
-        hash = attributes.each_with_object({}) do |(key, selector), attrs|
-          attrs[var_name] = page.css(selector).text.strip
-        end
-      end
     end
 
     def create_return_objects(name, attributes)
--- spec/proto/scraper_spec.rb
+++ spec/proto/scraper_spec.rb
@@ -1,18 +1,12 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Proto::Scraper do
-  before(:each) do
-    # Nokogiri::HTML.stub!(:open).and_return("doc")
-    # Nokogiri::HTML::Document.stub!(:parse)
-    # @scrape = Proto::Scraper.new('http://example.com')
-    # @scrape.stub_chain(:doc, :css, :each).and_return('STUBBED OUT')
-  end
-
   it 'returns my objects!' do
     obj = Proto::Scraper.new('https://twitter.com/kcurtin')
     obj_collection = obj.fetch('Tweet', { :name => 'strong.fullname',
-                                            :content => 'p.js-tweet-text', :created_at => 'small.time' })
-    # obj_collection.length.should == 10
+                                          :content => 'p.js-tweet-text', 
+                                          :created_at => 'small.time' }
+                              )
     obj_collection.first.class.to_s.should == 'Proto::Tweet'
     obj_collection.first.name.should == 'Kevin Curtin'
   end
@@ -22,45 +16,33 @@ describe Proto::Scraper do
       Proto::Scraper.new('blah_url')
     }.to raise_error(Errno::ENOENT)
   end
-  # context ".fetch" do
-  #   it "the default class name is 'Proto::Type'" do
-  #     our_obj = @scrape.fetch({})
-  #     our_obj.class.to_s.should == 'Proto::Type'
-  #   end
 
-  #   it "accepts only a hash and sets default class name" do
-  #     our_obj = @scrape.fetch({:name => 'default const'})
-  #     our_obj.class.to_s.should == 'Proto::Type'
-  #   end
+  it 'can collect a bunch of urls' do
+    obj = Proto::Scraper.new('http://jobs.rubynow.com/')
+    obj.collect_urls('ul.jobs li h2 a:first')
+    obj.url_collection.first.should =~ /http:\/\/jobs/
+  end
 
-  #   it "returns a Proto object with attributes set" do
-  #     our_obj = @scrape.fetch('Sample', {:name => "Kevin", :title => "Developer"})
-  #     our_obj.name.should == "STUBBED OUT"
-  #     our_obj.title.should == "STUBBED OUT"
-  #     our_obj.class.to_s.should == "Proto::Sample"
-  #   end
-  # end
-
-#   context 'private methods' do
-#     context ".create_return_objects" do
-#       it "accepts a custom class name" do
-#         our_obj = @scrape.send(:create_return_objects, 'Kevin', {})
-#         our_obj.first.class.to_s.should == 'Proto::Kevin'
-#       end
-
-#       it "accepts a hash and name and sets custom attrs" do
-#         our_obj = @scrape.send(:create_return_objects, 'Test', [{:name => 'Kevin'},{:title => "Title"}])
-#         our_obj.first.name.should == 'Kevin'
-#         our_obj.last.title.should == 'Title'
-#         our_obj.length.should == 2
-#       end
-#     end
-
-#     context ".scrape_attribute_data" do
-#       it "returns a hash of stuff" do
-#         rh = @scrape.send(:scrape_attribute_data, {:title => "h2 a"})
-#         rh.should == [{:title => 'STUBBED OUT'}]
-#       end
-#     end
-#   end
+  it "should create the objects this way too" do
+    obj = Proto::Scraper.new('http://jobs.rubynow.com/')
+    obj.collect_urls('ul.jobs li h2 a:first')
+    jobs = obj.fetch({  title:       'h2.jam_headline',
+                        company:     'h3 a.jam_link',
+                        location:    'div#c_address',
+                        type:        'div#c_jobtype',
+                        description: 'div#c_job_description' }
+                      )
+    jobs.first.should == 'test'
+  end
 end
+
+# ruby_inside = Scraper.new('Ruby Inside', 'http://ruby.jobamatic.com/a/jbb/find-jobs/', 
+#                           'http://ruby.jobamatic.com', job_database)
+# ruby_inside.compile_job_url_collection('tr.listing td.title a')
+# ruby_inside.scrape_away({
+#   title_text:       'h2.jam_headline',
+#   # company_text:     'h3 a.jam_link',
+#   location_text:    'div#c_address',
+#   type_text:         'div#c_jobtype',
+#   description_text: 'div#c_job_description'
+# })
